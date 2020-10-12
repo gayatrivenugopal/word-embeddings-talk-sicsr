b5db96b104892ba6bdc6db719974ffa2930f1a95
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2020-10-12 14:43:33 +0530

    train with graph

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 #Source: https://machinelearningmastery.com/develop-word-embeddings-python-gensim/
 
 from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
 # define training data
 sentences = [['यह', 'है', 'मेरा', 'पहला', 'वाक्य'],
 			['यह', 'है', 'मेरा', 'दूसरा', 'वाक्य'],
-			['यह', 'है', 'एक', 'नया' 'वाक्य'],
+			['यह', 'है', 'एक', 'नया', 'वाक्य'],
 			['एक','और', 'वाक्य'],
 			['और', 'एक', 'अंतिम', 'वाक्य']]
 # train model
@@ -19,5 +22,15 @@ print(model['वाक्य'])
 # save model
 model.save('model.bin')
 # load model
-new_model = Word2Vec.load('model.bin')
-print(new_model.most_similar(positive='वाक्य', topn=5))
+model = Word2Vec.load('model.bin')
+print(model.most_similar(positive='वाक्य', topn=5))
+X = model[model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+# create a scatter plot of the projection
+plt.rcParams['font.sans-serif'] = ['Lohit Devanagari']
+plt.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+	plt.annotate(word, xy=(result[i, 0], result[i, 1]))
+plt.show()
